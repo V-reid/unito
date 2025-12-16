72cbe987e8bdd50965f92660522e2d36ca340798
Wreid <venere.daniele01@gmail.com>
2025-12-16 11:51:52 +0100

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-	base: `/unito-deploy/`,
+	base: `/unito/`,
 	// server: { allowedHosts: ["94b286d5afb0.ngrok-free.app"] },
 	// server: {
 	// 	host: true,
